ef79aff782a8146f680709174d7b97d639cb6767
thegreatprogrammer <46078698+thegreatprogrammer@users.noreply.github.com>
2020-03-11 00:06:01 -0400

    Mejora en el código. Añadido de función changeActive()

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
 let parent = document.getElementById("parent");
 let navbar = document.getElementById("navbar");
 
+// FUNCTIONS
+
 //Función para crear un array a partir de un conjunto de elementos de una clase, y determinar cuál de ellos recibió click. Retorna el índice del elemento en cuestión.
 const indexArray = (e, parentElement, clase) =>{
     let indexArray = Array.from(parentElement.querySelectorAll(clase)),
@@ -10,17 +12,45 @@ const indexArray = (e, parentElement, clase) =>{
     return index;
 }
 
-const displayPoem = poemIndex =>{
-    let indexArray = Array.from(parent.children);
-    
-    indexArray.map(element => element.style.display = 'none');
+//Función encargada de modificar las clases active de los elementos del menú, a medida que el usuario hace click sobre cada uno
+const changeActive = (item) =>{
 
-    let pair = indexArray.filter(element => indexArray.indexOf(element) === poemIndex);
-    
-    pair[0].style.display = 'flex';
+    //Almacenando en un array todos los elementos a con clase nav-link, hijos de navbar
+    let nav = Array.from(navbar.querySelectorAll('a.nav-link'));
+    //Recorriendo el array, accediendo al elemento padre de cada elemento a (un elemento li).
+    //Se elimina la clase active de todos
+    nav.map(element => element.parentElement.classList.remove('active'));
+
+    //Se toma el elemento que coincide con el índice proporcionado (al cual el usuario hizo click), y se le añade la clase active.
+    nav[item].parentElement.classList += ' active';
 }
 
+//Esta función compara el índice del elemento seleccionado del menú, con los índices de los poemas. Muestra exclusivamente el poema cuyo índice coincida con el proporcionado.
+const displayPoem = poemIndex =>{
+
+    //Array con todos los elementos que conforman los poemas
+    let indexArray = Array.from(parent.children);
+
+    //Ocultando todos los elementos
+    indexArray.map(element => element.style.display = 'none');
+
+    //Filtrando el elemento cuyo índice coincida con el proporcionado
+    let pair = indexArray.filter(element => indexArray.indexOf(element) === poemIndex);
+    
+    //Mostrando el poema resultado del filtrado
+    pair[0].style.display = 'flex';
+
+}
+
+// EVENT
+
 navbar.addEventListener("click", e =>{
+
+    //Índice del item del menú al que se le hizo click
     let index = indexArray(e, navbar, 'a.nav-link');
-    displayPoem(index)
+
+    changeActive(index);
+
+    displayPoem(index);
+
 });
